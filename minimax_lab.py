8c77db75fce7d3c864886c390d60934e5aa28755
Jesus390 <jesusespinola90@gmail.com>
2025-05-20 10:18:20 -0300

    definir el tablero como una clase diferente

--- minimax_lab.py
+++ minimax_lab.py
@@ -1,15 +1,26 @@
-class TomyJerry:
+class Tablero:
     colores_del_cuadro = {'blanco': '⬜', 'negro': '⬛', 'azul': '🟦', 'verde': '🟩', 'morado': '🟪', 'amarillo': '🟨', 'rojo': '🟥', 'marron': '🟫', 'naranja': '🟧'}
+
+    def __init__(self, color_cuadro='blanco'):
+        self.cuadrado = color_cuadro
+
+    def crear(self, alto=10, largo=10):
+        '''
+        Crear un tablero de 'x' tamaño alto x largo con el cuadrado blanco en el centro.
+        '''
+        return (self.colores_del_cuadro[self.cuadrado] * largo + '\n') * alto
+
+
+class TomyJerry:
     emojis_gato = {'full':'🐈', 'cara': '🐱'}
     emojis_raton = {'raton':'🐁', 'cara':'🐭'}
 
-    def __init__(self, color_cuadro='blanco', emoji_gato='full', emoji_raton='full'):
-        self.cuadrado = color_cuadro
+    def __init__(self, emoji_gato='full', emoji_raton='full'):
         self.tom = emoji_gato
         self.jerry = emoji_raton
 
-    def print_tablero(self, alto=10, largo=10):
-        '''
-        Imprime un tablero de 'x' tamaño alto x largo con el cuadrado blanco en el centro.
-        '''
-        return (self.cuadrado['blanco'] * largo + '\n') * alto
+
+if __name__=='__main__':
+    t = Tablero()
+    print(t.crear())
+
