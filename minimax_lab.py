234b863594b284a520880f86737a3f1ebf96afc6
Jesus390 <jesusespinola90@gmail.com>
2025-05-20 11:29:29 -0300

    datos para cada casilla del tablero

--- minimax_lab.py
+++ minimax_lab.py
@@ -1,11 +1,13 @@
 class Tablero:
     colores = {'blanco': '⬜', 'negro': '⬛', 'azul': '🟦', 'verde': '🟩', 'morado': '🟪', 'amarillo': '🟨', 'rojo': '🟥', 'marron': '🟫', 'naranja': '🟧'}
-    tablero = {}
+    tablero = []
 
     def __init__(self, color='blanco', alto=10, largo=10):
         self.color = color
         self.alto = alto
         self.largo = largo
+        if self.alto <=2 or self.largo <=2:
+            raise("Error: El tablero debe tener un alto y un largo de al menos 3")
 
     def get_color(self):
         return self.color
@@ -31,16 +33,21 @@ class Tablero:
         '''
         for a in range(self.alto):
             for l in range(self.largo):
-                self.tablero[a] = {l: {
+                self.tablero.append({
                     'color_de_fondo': self.colores[self.color],
                     'posicion': (a, l),
-                    'ocupado': False
-                    }}
+                    'ocupado': False,
+                    })
         return self.tablero
     
     def imprimir(self):
+        print(f"Alto: {self.alto}\nLargo: {self.largo}\nLongitud: {len(self.tablero)}\n")
+        aux = ''
         for tablero in self.tablero:
-            print(tablero)
+            aux += tablero['color_de_fondo']
+            if tablero['posicion'][1] == self.largo -1:
+                aux += '\n'
+        print(aux)
         
 
 class TomyJerry:
