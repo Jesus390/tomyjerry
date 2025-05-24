85c3e9492ccbca88feba8778d4248a6cdfa62e25
Jesus390 <jesusespinola90@gmail.com>
2025-05-24 15:25:59 -0300

    método para obtener movimiento

--- game.py
+++ game.py
@@ -1,8 +1,23 @@
+from functions import *
 from tablero import Tablero
 
 class GameTomyJerry():
     def __init__(self):
         pass
+
+    def obtener_movimiento(self):
+        while True:
+            print('''
++ Movimientos:
+w - Arriba
+a - Izquierda
+d - Derecha
+s - Abajo
+''')
+            movimiento = input("Ingrese el movimiento: ")
+            if movimiento in ['w', 'a', 'd', 's']:
+                return movimiento
+
 
     def run(self):
         print("Bienvenido a GameTomyJerry")
