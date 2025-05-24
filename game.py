4619e96f26e1d5121a18675475aa8c9076387adf
Jesus390 <jesusespinola90@gmail.com>
2025-05-24 15:15:31 -0300

    creación de la clase GameTomyJerry

---
+++ game.py
@@ -0,0 +1,14 @@
+from tablero import Tablero
+
+class GameTomyJerry():
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Bienvenido a GameTomyJerry")
+        print("El juego se juega en un tablero de 10x10 casillas")
+        print("El jugador puede moverse en las cuatro direcciones")
+
+if __name__=="__main__":
+    juego = GameTomyJerry()
+    juego.run()
