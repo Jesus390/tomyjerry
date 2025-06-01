25047a513676c6a0abe2492ca121fdf5e2d848e1
Jesus390 <jesusespinola90@gmail.com>
2025-06-01 18:03:00 -0300

    se elimino codigo innecesario

--- tomyjerry/tablero.py
+++ tomyjerry/tablero.py
@@ -25,7 +25,6 @@ class Tablero:
             for l in range(self.largo):
                 fila.append({
                     'fondo': self.colores[self.color],
-                    'posicion': (a, l),
                     'ocupado': False,
                     })
             self.tablero.append(fila)
@@ -33,7 +32,6 @@ class Tablero:
         for entidad in self.endidades:
             self.tablero[entidad.posicion_x][entidad.posicion_y] = {
                 'fondo': entidad.emoji,
-                'posicion': (entidad.posicion_x, entidad.posicion_y),
                 'ocupado': True,
             }
 
@@ -42,12 +40,10 @@ class Tablero:
     def update(self, entidad):
         self.tablero[entidad.ultima_posicion_x][entidad.ultima_posicion_y] = {
             'fondo': self.colores[self.color],
-            'posicion': (entidad.ultima_posicion_x, entidad.ultima_posicion_y),
             'ocupado': False,
         }
         self.tablero[entidad.posicion_x][entidad.posicion_y] = {
             'fondo': entidad.emoji,
-            'posicion': (entidad.posicion_x, entidad.posicion_y),
             'ocupado': True,
         }
 
@@ -58,8 +54,7 @@ class Tablero:
         for filas in self.tablero:
             for elemento in filas:
                 aux += elemento['fondo']
-                if elemento['posicion'][1] == self.largo -1:
-                    aux += '\n'
+            aux += '\n'
         print(aux)
         
 
