e8638565457013d2fe428ef3802d19c049e8d246
Jesus390 <jesusespinola90@gmail.com>
2025-06-01 21:42:16 -0300

    funcion para ingresar por teclado los movimientos

--- tomyjerry/functions.py
+++ tomyjerry/functions.py
@@ -3,3 +3,9 @@ import os
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def obtener_movimiento():
+    while True:
+        print("+ Movimientos:\n(w)Arriba\n(a)Izquierda\n(d)Derecha\n(s)Abajo")
+        movimiento = input("Ingrese movimiento: ")
+        if movimiento in ['w', 'a', 'd', 's']:
+            return movimiento
