42e9be269e83a5e63ff7bfc1af3d25aa3aa07785
Jesus390 <jesusespinola90@gmail.com>
2025-06-02 15:47:56 -0300

    actualizacion...

--- tomyjerry/jugador.py
+++ tomyjerry/jugador.py
@@ -1,46 +1,24 @@
 from posicion import Posicion
 
 class Entidad():
-    pass
-
-class Posicion():
     def __init__(self, posicion_x, posicion_y):
         self.posicion_x = posicion_x
         self.posicion_y = posicion_y
         self.ultima_posicion_x = None
         self.ultima_posicion_y = None
 
-class Jugador(Entidad, Posicion):
-    def __init__(self, nombre, posicion_x, posicion_y):
+class Jugador(Entidad):
+    def __init__(self, nombre, posicion_x, posicion_y, tablero):
         super().__init__(posicion_x, posicion_y)
         self.nombre = nombre
+        self.tablero = tablero
         self.movimientos = [(1, 0), (-1, 0), (0, 1), (0, -1)] # para el minimax
 
-    def is_movimiento_valido(self, movimiento, tablero):
-        return 0 <= movimiento[0] < tablero.fila and 0 <= movimiento[1] < tablero.columna
-
-    def mover_derecha(self):
-        self.ultima_posicion_x = self.posicion_x
-        self.ultima_posicion_y = self.posicion_y
-        self.posicion_y = self.posicion_y + 1
-
-    def mover_izquierda(self):
-        self.ultima_posicion_x = self.posicion_x
-        self.ultima_posicion_y = self.posicion_y
-        self.posicion_y = self.posicion_y - 1
-    
-    def mover_abajo(self):
-        self.ultima_posicion_x = self.posicion_x
-        self.ultima_posicion_y = self.posicion_y
-        self.posicion_x = self.posicion_x + 1
-    
-    def mover_arriba(self):
-        self.ultima_posicion_x = self.posicion_x
-        self.ultima_posicion_y = self.posicion_y
-        self.posicion_x = self.posicion_x - 1
+    def is_movimiento_valido(self, movimiento):
+        return 0 <= movimiento[0] < self.tablero.fila and 0 <= movimiento[1] < self.tablero.columna
 
     def mover(self, movimiento, tablero):
-        if self.is_movimiento_valido(movimiento, tablero):
+        if self.is_movimiento_valido(movimiento):
             self.posicion_x = self.posicion_x + movimiento[0]
             self.posicion_y = self.posicion_y + movimiento[1]
         
@@ -58,21 +36,21 @@ class Jugador(Entidad, Posicion):
 class Raton(Jugador):
     emojis = {'full':'🐁', 'cara':'🐭'}
 
-    def __init__(self, pos_x, pos_y, nombre='Jerry', emoji='full'):
-        super().__init__(nombre, pos_x, pos_y)
+    def __init__(self, pos_x, pos_y, tablero, nombre='Jerry', emoji='full'):
+        super().__init__(nombre, pos_x, pos_y, tablero)
         self.emoji = self.emojis[emoji]
 
 class Gato(Jugador):
     emojis = {'full':'🐈', 'cara': '🐱'}
     
-    def __init__(self, pos_x, pos_y, nombre='Tom', emoji='full'):
-        super().__init__(nombre, pos_x, pos_y)
+    def __init__(self, pos_x, pos_y, tablero, nombre='Tom', emoji='full'):
+        super().__init__(nombre, pos_x, pos_y, tablero)
         self.emoji = self.emojis[emoji]
 
     def __str__(self):
         return f"Class {self.nombre} {self.emoji}"
     
-class Obstaculo(Entidad, Posicion):
+class Obstaculo(Entidad):
     pass
 
 class Pared(Obstaculo):
