585d8de02819e0b272c7103bb4f2c5c7cc43d396
Jesus390 <jesusespinola90@gmail.com>
2025-06-09 19:02:11 -0300

    se agrego la variable tablero como una variable de instancia

--- tomyjerry/tablero.py
+++ tomyjerry/tablero.py
@@ -1,41 +1,36 @@
 class Tablero:
     colores = {'default': ' . ', 'blanco': '⬜', 'negro': '⬛', 'azul': '🟦', 'verde': '🟩', 'morado': '🟪', 'amarillo': '🟨', 'rojo': '🟥', 'marron': '🟫', 'naranja': '🟧'}
-    tablero = []
-    endidades = []
 
     def __init__(self, color='default', fila=10, columna=10):
         self.color = color
         self.fila = fila
         self.columna = columna
-        if self.fila < 5:
-            raise("Error: El tablero debe tener un fila de al menos 5")
-        if self.columna < 5:
-            raise("Error: El tablero debe tener un columna de al menos 5")
+        self.tablero = [['' for _ in range(fila)] for _ in range(columna)]
 
     def agregar_entidad(self, entidad):
         self.endidades.append(entidad)
 
 
-    def crear(self):
-        '''
-        Crear un tablero de 'x' tamaño fila x columna con el cuadrado blanco en el centro.
-        '''
-        for a in range(self.fila):
-            fila = []
-            for l in range(self.columna):
-                fila.append({
-                    'fondo': self.colores[self.color],
-                    'ocupado': False,
-                    })
-            self.tablero.append(fila)
+    # def crear(self):
+    #     '''
+    #     Crear un tablero de 'x' tamaño fila x columna con el cuadrado blanco en el centro.
+    #     '''
+    #     for a in range(self.fila):
+    #         fila = []
+    #         for l in range(self.columna):
+    #             fila.append({
+    #                 'fondo': self.colores[self.color],
+    #                 'ocupado': False,
+    #                 })
+    #         self.tablero.append(fila)
         
-        for entidad in self.endidades:
-            self.tablero[entidad.posicion_x][entidad.posicion_y] = {
-                'fondo': entidad.emoji,
-                'ocupado': True,
-            }
+    #     for entidad in self.endidades:
+    #         self.tablero[entidad.posicion_x][entidad.posicion_y] = {
+    #             'fondo': entidad.emoji,
+    #             'ocupado': True,
+    #         }
 
-        return self.tablero
+    #     return self.tablero
     
     def update(self, entidad):
         self.tablero[entidad.ultima_posicion_x][entidad.ultima_posicion_y] = {
@@ -57,3 +52,6 @@ class Tablero:
             aux += '\n'
         print(aux)
         
+if __name__ == "__main__":
+    tablero = Tablero()
+    print(tablero.tablero)
