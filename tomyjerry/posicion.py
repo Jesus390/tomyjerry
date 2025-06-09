272deecb70f82cc0e63dcef88ad4d02895978c95
Jesus390 <jesusespinola90@gmail.com>
2025-06-09 20:37:16 -0300

    se elimina el archivo posicion.py

--- tomyjerry/posicion.py
+++
@@ -1,6 +0,0 @@
-class Posicion():
-    def __init__(self, posicion_x, posicion_y):
-        self.ultima_posicion_x = None
-        self.ultima_posicion_y = None
-        self.posicion_x = posicion_x
-        self.posicion_y = posicion_y
