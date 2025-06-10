5862bf98c7a2ea37b8e47f65424b03c9387760f2
Jesus390 <jesusespinola90@gmail.com>
2025-06-09 23:04:34 -0300

    corrección movimientos

--- tomyjerry/jugador.py
+++ tomyjerry/jugador.py
@@ -10,14 +10,26 @@ class Jugador(Entidad):
         super().__init__(fila, columna)
         self.movimientos = [(1, 0), (-1, 0), (0, 1), (0, -1)] # para el minimax
 
-    def calcular_movimiento(self, fila, columna):
-        return self.fila+fila, self.columna+columna
+    def calcular_movimiento(self, movimiento):
+        return self.fila+movimiento[0], self.columna+movimiento[1]
 
     def mover(self, fila, columna):
         self.ultima_columna = self.columna
         self.ultima_fila = self.fila
         self.columna = columna
         self.fila = fila
+
+    def mover_arriba(self):
+        return (self.movimientos[1][0], self.movimientos[1][1])
+    
+    def mover_abajo(self):
+        return (self.movimientos[0][0], self.movimientos[0][1])
+    
+    def mover_izquierda(self):
+        return (self.movimientos[3][0], self.movimientos[3][1])
+    
+    def mover_derecha(self):
+        return (self.movimientos[2][0], self.movimientos[2][1])
 
     def movimientos_disponibles(self, tablero):
         return [(posicion[0], posicion[1]) for posicion in self.movimientos if tablero.is_inTablero(self.fila+posicion[0], self.columna+posicion[1])]
